b87c33988bb6d23d0afb0c829265f50d4ca13241
Misurida <erwan.guyomarch@gmail.com>
2019-07-16 23:05:21 +0200

    Skin and hair colors

---
+++ hair_colors.js
@@ -0,0 +1,156 @@
+const hair_colors = [{
+    name: "Midnight Black",
+    code: "",
+    rgb: "#090806",
+    type: "Black"
+},{
+    name: "Off Black",
+    code: "",
+    rgb: "#2C222B",
+    type: "Black"
+},{
+    name: "Darkest Brown",
+    code: "",
+    rgb: "#3B3C24",
+    type: "Brown"
+},{
+    name: "Chestnut Brown",
+    code: "",
+    rgb: "#504444",
+    type: "Brown"
+},{
+    name: "Light Chestnut Brown",
+    code: "",
+    rgb: "#6A4E42",
+    type: "Dark brown"
+},{
+    name: "Dark golden Brown",
+    code: "",
+    rgb: "#554838",
+    type: "Brown"
+},{
+    name: "Light Golden Brown",
+    code: "",
+    rgb: "#A7856A",
+    type: "Brown"
+},{
+    name: "Dark Honey Blonde",
+    code: "",
+    rgb: "#B89778",
+    type: "Blonde"
+},{
+    name: "Bleached Blonde",
+    code: "",
+    rgb: "#DCD0BA",
+    type: "Blonde"
+},{
+    name: "Light Ash Blonde",
+    code: "",
+    rgb: "#DEBC99",
+    type: "Blonde"
+},{
+    name: "Light Ash Brown",
+    code: "",
+    rgb: "#97961",
+    type: "Brown"
+},{
+    name: "Lightest Blonde",
+    code: "",
+    rgb: "#E6CEA8",
+    type: "Blonde"
+},{
+    name: "Pale Golden Blonde",
+    code: "",
+    rgb: "#E5C8A8",
+    type: "Blonde"
+},{
+    name: "Strawberry Blonde",
+    code: "",
+    rgb: "#A56B46",
+    type: "Brown"
+},{
+    name: "Light Auburn",
+    code: "",
+    rgb: "#91553D",
+    type: "Brown"
+},{
+    name: "Dark Auburn",
+    code: "",
+    rgb: "#533D32",
+    type: "Brown"
+},{
+    name: "Darkest Gray",
+    code: "",
+    rgb: "#71635A",
+    type: "Gray"
+},{
+    name: "Medium Gray",
+    code: "",
+    rgb: "#B7A69E",
+    type: "Gray"
+},{
+    name: "Light Gray",
+    code: "",
+    rgb: "#D6C4C2",
+    type: "Gray"
+},{
+    name: "White Blonde",
+    code: "",
+    rgb: "#FFF5E1",
+    type: "Blonde"
+},{
+    name: "Platinium Blonde",
+    code: "",
+    rgb: "#CBBFB1",
+    type: "Blonde"
+},{
+    name: "Russet Red",
+    code: "",
+    rgb: "#8D4A42",
+    type: "Ginger"
+},{
+    name: "Terra Cotta",
+    code: "",
+    rgb: "#B6523A",
+    type: "Ginger"
+},{
+    name: "Toasted Wheat",
+    code: "",
+    rgb: "#D8C078",
+    type: "Blonde"
+},{
+    name: "Melted Butter",
+    code: "",
+    rgb: "#E3CC88",
+    type: "Blonde"
+},{
+    name: "Wheat Milk",
+    code: "",
+    rgb: "#F2DA91",
+    type: "Blonde"
+},{
+    name: "Cake Two",
+    code: "",
+    rgb: "#F2E1AE",
+    type: "Blonde"
+},{
+    name: "Poor Jean",
+    code: "",
+    rgb: "#F2E7C7",
+    type: "Blonde"
+},{
+    name: "Mud on my Shoe",
+    code: "",
+    rgb: "#664F3C",
+    type: "Brown"
+},{
+    name: "Cookie",
+    code: "",
+    rgb: "#8C684A",
+    type: "Brown"
+},{
+    name: "Tree Bark",
+    code: "",
+    rgb: "#332A22",
+    type: "Brown"
+}];
---
+++ skin_colors.js
@@ -0,0 +1,150 @@
+const skin_colors = [{
+    type: "red",
+    name: "Red 1",
+    code: "#995566"
+},{
+    type: "red",
+    name: "Red 2",
+    code: "#AA6666"
+},{
+    type: "mocha",
+    name: "Mocha 1",
+    code: "#B59987"
+},{
+    type: "mocha",
+    name: "Mocha 2",
+    code: "#BCA99D"
+},{
+    type: "mocha",
+    name: "Mocha 3",
+    code: "#C6BAB5"
+},{
+    type: "mocha",
+    name: "Mocha 4",
+    code: "#B59C93"
+},{
+    type: "pink",
+    name: "Pink 1",
+    code: "#F7E1D3"
+},{
+    type: "pink",
+    name: "Pink 2",
+    code: "#F0D0CB"
+},{
+    type: "pink",
+    name: "Pink 3",
+    code: "#EEC5BF"
+},{
+    type: "pink",
+    name: "Pink 4",
+    code: "#EEBDB7"
+},{
+    type: "pink",
+    name: "Pink 5",
+    code: "#EEB4B0"
+},{
+    type: "white",
+    name: "White 1",
+    code: "#FDC5B1"
+},{
+    type: "white",
+    name: "White 2",
+    code: "#FBD4C5"
+},{
+    type: "white",
+    name: "White 3",
+    code: "#FBDDCF"
+},{
+    type: "white",
+    name: "White 4",
+    code: "#FDE7DD"
+},{
+    type: "white",
+    name: "White 5",
+    code: "#FDEFE5"
+},{
+    type: "brown",
+    name: "Brown 1",
+    code: "#9F5C3C"
+},{
+    type: "brown",
+    name: "Brown 2",
+    code: "#B87152"
+},{
+    type: "brown",
+    name: "Brown 3",
+    code: "#C18566"
+},{
+    type: "brown",
+    name: "Brown 4",
+    code: "#CA9072"
+},{
+    type: "brown",
+    name: "Brown 5",
+    code: "#D3A186"
+},{
+    type: "brown",
+    name: "Brown 6",
+    code: "#D8A994"
+},{
+    type: "black",
+    name: "Black 1",
+    code: "#260701"
+},{
+    type: "black",
+    name: "Black 2",
+    code: "#3D0C02"
+},{
+    type: "black",
+    name: "Black 3",
+    code: "#843722"
+},{
+    type: "black",
+    name: "Black 4",
+    code: "#C69076"
+},{
+    type: "black",
+    name: "Black 5",
+    code: "#D8A994"
+},{
+    type: "yellow",
+    name: "Yellow 1",
+    code: "#FFE0BD"
+},{
+    type: "yellow",
+    name: "Yellow 2",
+    code: "#FFCD94"
+},{
+    type: "yellow",
+    name: "Yellow 3",
+    code: "#EAC086"
+},{
+    type: "yellow",
+    name: "Yellow 4",
+    code: "#FFAD60"
+},{
+    type: "yellow",
+    name: "Yellow 5",
+    code: "#FFE39F"
+}];
+
+
+const skin_types_colors = [{
+    name: 'Light',
+    code: '#F6D9CB',
+},{
+    name: 'Caucasian',
+    code: '#EFC0A4',
+},{
+    name: 'Tanned',
+    code: '#D68D6A',
+},{
+    name: 'Asian',
+    code: '#edc582',
+},{
+    name: 'Brown',
+    code: '#C98558',
+},{
+    name: 'Black',
+    code: '#5B3C28',
+}];
