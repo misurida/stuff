224837b1c2a3ddf862d7e4b589b83f9e47cb8d73
Misurida <erwan.guyomarch@gmail.com>
2019-07-16 16:11:42 +0200

    Comsumables

---
+++ alcools.js
@@ -0,0 +1,76 @@
+const alcools = [{
+    category: "Léger",
+    type: "Bière"
+},{
+    category: "Medium",
+    type: "Vin rouge"
+},{
+    category: "Medium",
+    type: "Vin blanc"
+},{
+    category: "Medium",
+    type: "Vin rosé"
+},{
+    category: "Medium",
+    type: "Champagne"
+},{
+    category: "Medium",
+    type: "Alcool anisé"
+},{
+    category: "Alcool fort",
+    type: "Whisky"
+},{
+    category: "Alcool fort",
+    type: "Rhum brun"
+},{
+    category: "Alcool fort",
+    type: "Rhum blanc"
+},{
+    category: "Alcool fort",
+    type: "Vodka"
+},{
+    category: "Alcool fort",
+    type: "Sambuca"
+},{
+    category: "Alcool fort",
+    type: "Tequila"
+},{
+    category: "Liqueur",
+    type: "Liqueur de fruit"
+},{
+    category: "Liqueur",
+    type: "Crème de fruit"
+},{
+    category: "Liqueur",
+    type: "Liqueur amère"
+},{
+    category: "Liqueur",
+    type: "Chartreuse"
+},{
+    category: "Liqueur",
+    type: "Absinthe"
+},{
+    category: "Liqueur",
+    type: "Amaretto"
+},{
+    category: "Liqueur",
+    type: "Eaux-de-vie"
+},{
+    category: "Liqueur",
+    type: "Kirsh"
+},{
+    category: "Liqueur",
+    type: "Kahlua"
+},{
+    category: "Liqueur",
+    type: "Cognac"
+},{
+    category: "Liqueur",
+    type: "Brandy"
+},{
+    category: "Cocktail",
+    type: "Cocktail"
+},{
+    category: "Cocktail",
+    type: "Punch"
+}];
---
+++ weeds.js
@@ -0,0 +1,271 @@
+const weeds = [{
+    name: "XXX OG",
+    prefix: "XXX",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "92 OG",
+    prefix: "92",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Billy Kimber OG",
+    prefix: "Killy Kimber",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Wedding Cake",
+    prefix: "Wedding",
+    suffix: "Cake",
+    type: "hybrid"
+},{
+    name: "Sugar Biscuits #8",
+    prefix: "Sugar",
+    suffix: "Biscuits #8",
+    type: "hybrid"
+},{
+    name: "Tangimal Cookies",
+    prefix: "Tangimal",
+    suffix: "Cookies",
+    type: "hybrid"
+},{
+    name: "Dosi-Orange #9",
+    prefix: "Dosi-Orange",
+    suffix: "#9",
+    type: "sativa"
+},{
+    name: "Peach ringz",
+    prefix: "Peach",
+    suffix: "ringz",
+    type: "sativa"
+},{
+    name: "J1",
+    prefix: "J1",
+    suffix: null,
+    type: "sativa"
+},{
+    name: "Motorbreath",
+    prefix: "Motorbreath",
+    suffix: null,
+    type: "indica"
+},{
+    name: "Urbols Skywalker",
+    prefix: "Urbols",
+    suffix: "Skywalker",
+    type: "indica"
+},{
+    name: "CFK OG Story Kush",
+    prefix: "CFK OG",
+    suffix: "Story Kush",
+    type: "indica"
+},{
+    name: "El Perro",
+    prefix: "El",
+    suffix: "Perro",
+    type: "hybrid"
+},{
+    name: "Mega Sherbert",
+    prefix: "Mega",
+    suffix: "Sherbert",
+    type: "hybrid"
+},{
+    name: "Holy Fire Knockout OG",
+    prefix: "Holy Fire",
+    suffix: "Knockout OG",
+    type: "hybrid"
+},{
+    name: "Orange Tree",
+    prefix: "Orange",
+    suffix: "Tree",
+    type: "sativa"
+},{
+    name: "Pineapple Sage",
+    prefix: "Pineapple",
+    suffix: "Sage",
+    type: "sativa"
+},{
+    name: "LA Snow",
+    prefix: "LA",
+    suffix: "Snow",
+    type: "sativa"
+},{
+    name: "Grape Stomper OG",
+    prefix: "Grape Stomper",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Kushco OG",
+    prefix: "Kushco",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Jah Cookies",
+    prefix: "Jah",
+    suffix: "Cookies",
+    type: "indica"
+},{
+    name: "CannaSutra OG",
+    prefix: "CannaSutra",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Dizzy OG",
+    prefix: "Dizzy",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Starkiller OG",
+    prefix: "Starkiller",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Mega Queso",
+    prefix: "Mega",
+    suffix: "Queso",
+    type: "sativa"
+},{
+    name: "You Don't Know Jack",
+    prefix: "You Don't Know",
+    suffix: "Kack",
+    type: "sativa"
+},{
+    name: "Purple Tangie",
+    prefix: "Purple",
+    suffix: "Tangie",
+    type: "sativa"
+},{
+    name: "Kosher Kush",
+    prefix: "Kosher",
+    suffix: "Kush",
+    type: "indica"
+},{
+    name: "Lorax OG",
+    prefix: "Lorax",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Dr. K's Paris OG Kush",
+    prefix: "Dr. K's",
+    suffix: "Paris OG Kush",
+    type: "indica"
+},{
+    name: "Wellness OG",
+    prefix: "Wellness",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Orange Cookies",
+    prefix: "Orange",
+    suffix: "Cookies",
+    type: "hybrid"
+},{
+    name: "2010 Grizzly Genetics",
+    prefix: "2010 Grizzly",
+    suffix: "Genetics",
+    type: "hybrid"
+},{
+    name: "Mango Tango",
+    prefix: "Mango",
+    suffix: "Tango",
+    type: "sativa"
+},{
+    name: "Loud Berry Flower",
+    prefix: "Loud",
+    suffix: "Berry Flower",
+    type: "sativa"
+},{
+    name: "Tangie",
+    prefix: "Tangie",
+    suffix: null,
+    type: "sativa"
+},{
+    name: "XXX OG",
+    prefix: "XXX OG",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Alpha OG",
+    prefix: "Alpha",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Redding OG",
+    prefix: "Redding",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Gorilla Glue #4",
+    prefix: "Gorilla Glue",
+    suffix: "#4",
+    type: "hybrid"
+},{
+    name: "Alpha OG",
+    prefix: "Alpha",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Redding OG",
+    prefix: "Redding",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Red Dragon",
+    prefix: "Red",
+    suffix: "Dragon",
+    type: "sativa"
+},{
+    name: "Silver Haze",
+    prefix: "Silver",
+    suffix: "Haze",
+    type: "sativa"
+},{
+    name: "Crystal Coma",
+    prefix: "Crystal",
+    suffix: "Coma",
+    type: "sativa"
+},{
+    name: "Godfather OG",
+    prefix: "Godfather",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "OG Eddy Lepp",
+    prefix: "OG",
+    suffix: "Eddy Lepp",
+    type: "indica"
+},{
+    name: "Grimace OG",
+    prefix: "Grimace",
+    suffix: "OG",
+    type: "indica"
+},{
+    name: "Girl Scout Cookie",
+    prefix: "Girl Scout",
+    suffix: "Cookie",
+    type: "hybrid"
+},{
+    name: "Vegan Tahoe OG",
+    prefix: "Vegan Tahoe",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Banana OG",
+    prefix: "Banana",
+    suffix: "OG",
+    type: "hybrid"
+},{
+    name: "Tanj",
+    prefix: "Tanj",
+    suffix: null,
+    type: "sativa"
+},{
+    name: "Veganic Ghost Train Haze #1",
+    prefix: "Veganic Ghost Train",
+    suffix: "Haze #1",
+    type: "sativa"
+},{
+    name: "Red Congolese",
+    prefix: "Red",
+    suffix: "Congolese",
+    type: "sativa"
+}];
